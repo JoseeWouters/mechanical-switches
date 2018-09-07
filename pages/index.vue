4c1bcec5a644be4a15eb4d02abe9ad53fa5dc3d6
Josee <info@joseewouters.nl>
2018-09-07 17:26:51 +0200

    First setup with a few switches and one filter

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,64 @@
 <template>
-    <h1>Hello World!</h1>
+    <section>
+        <ul>
+            <li v-for="item in filteredSwitches" v-bind:key="item.id">
+                {{item.name}}
+            </li>
+        </ul>
+        <h1>Type switch</h1>
+        <label for="linear">
+            Linear
+            <input type="checkbox" id="linear" v-model="filterOptions" value="linear">
+        </label>
+        <label for="clicky">
+            Clicky
+            <input type="checkbox" id="clicky" v-model="filterOptions" value="clicky">
+        </label>
+        <label for="tactile">
+            Tactile
+            <input type="checkbox" id="tactile" v-model="filterOptions" value="tactile">
+        </label>
+    </section>
 </template>
+
+<script>
+
+export default {
+    data: () => {
+        return {
+            filterOptions: [],
+            switches: [
+                {
+                    name: 'Cherry MX Red',
+                    type: 'linear',
+                    force: 45,
+                    point: 2,
+                },
+                {
+                    name: 'Cherry MX Brown',
+                    type: 'tactile',
+                    force: 45,
+                    point: 2,
+                },
+                {
+                    name: 'Cherry MX Blue',
+                    type: 'clicky',
+                    force: 60,
+                    point: 2,
+                }
+            ]
+        }
+    },
+    computed: {
+        filteredSwitches() {
+            if (!this.filterOptions.length) {
+		  		return this.switches;
+	  		} else {
+				return this.switches.filter(item => {
+		  			return this.filterOptions.includes(item.type);
+				});
+	  		} // else
+        }
+    }
+}
+</script>
